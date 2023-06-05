a98804eafb698d7190f96c7040b0a65552bbdd0b
joaop <joaop@lacunasoftware.com>
2023-06-05 19:17:25 -0300

    Fix frontend api calls

--- hourglass-frontend/src/app/shared/services/authentication.service.ts
+++ hourglass-frontend/src/app/shared/services/authentication.service.ts
@@ -1,6 +1,6 @@
 import { AuthResponseDto } from './../../_interfaces/response/authResponseDto.model';
 import { Injectable } from '@angular/core';
-import { RegistrationRequest } from './../../_interfaces/user/userForRegistrationDto.model'; 
+import { RegistrationRequest } from './../../_interfaces/user/userForRegistrationDto.model';
 import { RegistrationResponseDto } from './../../_interfaces/response/registrationResponseDto.model';
 import { HttpClient } from '@angular/common/http';
 import { EnvironmentUrlService } from './environment-url.service';
@@ -20,7 +20,7 @@ export class AuthenticationService {
   constructor(private http: HttpClient, private envUrl: EnvironmentUrlService, private jwtHelper: JwtHelperService) { }
 
   public registerUser = (route: string, body: RegistrationRequest) => {
-    return this.http.post<RegistrationResponseDto> (this.createCompleteRoute(route, this.envUrl.urlAddress), body);
+    return this.http.post<RegistrationResponseDto>(this.createCompleteRoute(route, this.envUrl.urlAddress), body);
   }
 
   public loginUser = (route: string, body: UserForAuthenticationDto) => {
@@ -28,37 +28,37 @@ export class AuthenticationService {
   }
 
   public forgotPassword = (route: string, body: ForgotPasswordDto) => {
-    return this.http.post(this.createCompleteRoute(route, this.envUrl.urlAddress), body);
-  }
+    return this.http.post(this.createCompleteRoute(route, this.envUrl.urlAddress), body, { responseType: 'text' });
+}
 
   public resetPassword = (route: string, body: ResetPasswordRequest) => {
-    return this.http.post(this.createCompleteRoute(route, this.envUrl.urlAddress), body);
-  }
+  return this.http.post(this.createCompleteRoute(route, this.envUrl.urlAddress), body, { responseType: 'text' });
+}
 
   public sendAuthStateChangeNotification = (isAuthenticated: boolean) => {
-    this.authChangeSub.next(isAuthenticated);
-  }
+  this.authChangeSub.next(isAuthenticated);
+}
 
   public logout = () => {
-    localStorage.removeItem("token");
-    this.sendAuthStateChangeNotification(false);
-  }
+  localStorage.removeItem("token");
+  this.sendAuthStateChangeNotification(false);
+}
 
   public isUserAuthenticated = (): boolean => {
-    const token = localStorage.getItem("token");
- 
-    return token && !this.jwtHelper.isTokenExpired(token);
-  }
+  const token = localStorage.getItem("token");
+
+  return token && !this.jwtHelper.isTokenExpired(token);
+}
 
   public isUserAdmin = (): boolean => {
-    const token = localStorage.getItem("token");
-    const decodedToken = this.jwtHelper.decodeToken(token);
-    const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
-    
-    return role === 'Administrator';
-  }
+  const token = localStorage.getItem("token");
+  const decodedToken = this.jwtHelper.decodeToken(token);
+  const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']
+
+  return role === 'Administrator';
+}
 
   private createCompleteRoute = (route: string, envAddress: string) => {
-    return `${envAddress}/${route}`;
-  }
+  return `${envAddress}/${route}`;
+}
 }
