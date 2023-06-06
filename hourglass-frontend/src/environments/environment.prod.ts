5db372c7d7688b4aa3a37142048442407735f8c1
joaop <joaop@lacunasoftware.com>
2023-06-06 10:50:33 -0300

    change client uri

--- hourglass-frontend/src/environments/environment.prod.ts
+++ hourglass-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlAddress: 'http://hourglass.codando.engineer:8080',
-  clientURI: 'https://witty-tree-0a7c11110.3.azurestaticapps.net'
+  clientURI: 'https://hourglassapp.azurewebsites.net'
 };
