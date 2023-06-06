a7b1956855b135e5537c9e47e2e717f0289361b9
joaop <joaop@lacunasoftware.com>
2023-06-06 13:53:19 -0300

    .

--- hourglass-frontend/src/environments/environment.prod.ts
+++ hourglass-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlAddress: 'http://hourglass.codando.engineer:8080',
+  urlAddress: 'https://hourglass.codando.engineer:8080',
   clientURI: 'https://hourglassapp.azurewebsites.net'
 };
